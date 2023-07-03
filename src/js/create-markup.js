2e0115a12c031658420d57388245d8571d1e1cfa
fehmituran <86859577+fehmituran@users.noreply.github.com>
2023-07-03 18:30:47 -0400

    modal css is completed
    modal css is completed

--- src/js/create-markup.js
+++ src/js/create-markup.js
@@ -44,72 +44,99 @@ export function createEventInfo(event) {
     price = `${event.priceRanges[0].min} - ${event.priceRanges[0].max} ${event.priceRanges[0].currency}`;
   }
 
+
   const eventInfo = `
-    <div class="smallimg">
-    <img
-  class="modal__img-s"
-  src="${event.images[5].url}" 
-  alt="${event.name}"
-  width="150"
-  </div>
-  <div class="event-info">
-    <img
-  class="modal__img-l"
-  src="${event.images[0].url}" 
-  alt="${event.name}"
-  width="427"
-/>
-<div class="info">
-  <div class="info-item">
-    <h4>INFO</h4>
-    
-    <p>${event.info}</p>
-  </div>
-  <div class="info-item">
-    <h4>WHEN</h4>
-    <span>${event.dates.start.localDate} (${event._embedded.venues[0].city.name} / ${event._embedded.venues[0].state.name}) </span>
-  </div>
-  <div class="info-item">
-    <h4>WHERE</h4>
-    <span>${event._embedded.venues[0].city.name}, ${event._embedded.venues[0].state.name} </span></br>
-    <span>${event._embedded.venues[0].country.name} </span>
-  </div>
-</div>
-  </div>
-  
-  <div class="who">
-    <h4>WHO</h4>
-    <span>${event._embedded.attractions[0].name} </span>
-  </div>
+  <div class="small__img">
+        <img
+          class="modal__img__small"
+          src="${event.images[5].url}" 
+            alt="${event.name}"
+          width="150"
+        />
+      </div>
 
-  <div class="prices">
-    <div class="price">
-      <h4>price</h4>
-    </div>
-    <div class="standart">
+      <div class="event__details">
+        <div class="large__img">
+          <img
+          class="modal__img__large"
+          src="${event.images[5].url}" 
+            alt="${event.name}"
+          width="427"
+        />
+        </div>
+
+        <div class="info__detail">
+          <div class="info__item">
+            <h3 class="info__item__title" >INFO</h3>
+            <p>${event.info}</p>
+          </div>
+          <div class="info__item">
+            <h3 class="info__item__title" >WHEN</h3>
+            <span>${event.dates.start.localDate} (${event._embedded.venues[0].city.name} / ${event._embedded.venues[0].state.name}) </span>
+          </div>
+          <div class="info__item">
+            <h3 class="info__item__title" >WHERE</h3>
+            <span>${event._embedded.venues[0].city.name}, ${event._embedded.venues[0].state.name}</br>>${event._embedded.venues[0].country.name}  </span>
+          </div>
+        
+          </div>
+
+        <div class="info__item">
+          <h3 class="info__item__title who__info" >WHO</h3>
+          <span>${event._embedded.attractions[0].name} </span>
+      </div>
       
-      <span>|||||</span> STANDART ${price}
-      </p>
-      <a href=${event.url} target="_blank">
-      <button class="modal__buy-btn">BUY TICKETS</button>
+      <div class="price__info">
+        <h3 class="info__item__title" >PRICES</h3>
+        
+        <div class="price__detail">
+
+        <div class="price__standart">
+          <div class="price__text">
+          <span class="price__icon">
+          ${price__icon} </span>  <span class="info__item__text">Standart ${price} </span> 
+          </div>
+          <a href=${event.url} target="_blank">
+            <button class="ticket__btn">BUY TİCKETS</button>
+           </a>
+          
+        </div>
+
+        <div class="price__vip">
+          <div class="price__text">
+          <span class="price__icon">
+          ${price__icon} </span>
+          <span class="info__item__text">Vip  ${price}</span> 
+          </div>
+          <a href=${event.url} target="_blank">
+          
+            <button class="ticket__btn">BUY TİCKETS</button>
+            
+           </a>
+        </div>
+      </div>
+    </div>    
+      </div>
+
+    <div class="more__info">
+      <a href=${event.url}  target="_blank">
+      <button class="more__info__btn" >MORE FROM THİS AUTHOR</button>
      </a>
+        
+      </div>
 
-    </div>
-    <div class="vip">
-    <span>|||||</span> VIP ${price}</p>
-    <a href=${event.url} target="_blank">
-    <button class="modal__buy-btn">BUY TICKETS</button>
-   </a>
-    </div>
-  </div>
-
-  <div class="moreinfo">
-  <a href=${event.url} target="_blank">
-  <button>More From This AUTHOR</button>
- </a>
-    
-  </div>
-    `;
+  `;
+   
 
   refs.contentEvent.insertAdjacentHTML('beforeend', eventInfo);
 }
+
+
+const price__icon = `<svg width="29" height="19" viewBox="0 0 29 19"
+            xmlns="http://www.w3.org/2000/svg"><path d="M4.91 1.27h-4.91v29.46h4.91v-29.46z"></path>
+            <path d="M17.26 1.27h-4.91v29.46h4.91v-29.46z"></path>
+            <path d="M24.699 1.27h-4.91v29.46h4.91v-29.46z"></path>
+            <path d="M44.19 1.27h-7.291v29.46h7.291v-29.46z"></path>
+            <path d="M9.82 1.27h-2.381v29.46h2.381v-29.46z"></path>
+            <path d="M29.46 1.27h-2.381v29.46h2.381v-29.46z"></path>
+            <path d="M34.37 1.27h-2.381v29.46h2.381v-29.46z"></path></svg>`;
